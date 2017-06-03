41bc999c0953f7f6fa76345552d25b1eff12cb19
Matt Hicks <matt@outr.com>
2017-06-03 10:27:34 -0500

    More functionality.

--- src/main/scala/com/outr/psd/PSD.scala
+++ src/main/scala/com/outr/psd/PSD.scala
@@ -17,14 +17,22 @@ object PSD extends js.Object {
 
 @ScalaJSDefined
 trait PSD extends js.Object {
-  def tree(): PSDTree
   def image: PSDImage
+
+  def file: PSDFile
+  def parsed: Boolean
+  def header: PSDHeader
+  def resources: js.Object
+  def layerMask: js.Object
+
+  def tree(): PSDTree
 }
 
 @ScalaJSDefined
 trait PSDTree extends js.Any {
   def export(): js.Object
-  def descendants(): js.Array[PSDLayer]
+  def descendants(): js.Array[PSDNode]
+  def children(): js.Array[PSDNode]
 }
 
 @ScalaJSDefined
@@ -33,7 +41,128 @@ trait PSDImage extends js.Any {
 }
 
 @ScalaJSDefined
-trait PSDLayer extends js.Any {
-  def export(): js.Object
+trait PSDNode extends js.Object {
+  def layer: PSDLayer
+  def parent: js.Object
+  def _children: js.Array[PSDNode]
+  def name: String
+  def forceVisible: Boolean
+  def coords: js.Object
+  def topOffset: js.Object
+  def leftOffset: js.Object
+
+  def children(): js.Array[PSDNode]
+  def isGroup(): Boolean
+  def export(): PSDNodeExport
   def toPng(): html.Image
+}
+
+@ScalaJSDefined
+trait PSDLayer extends js.Object {
+  def file: js.Object
+  def header: js.Object
+  def mask: js.UndefOr[PSDMask]
+  def blendingRanges: js.Object
+  def adjustments: js.Object
+  def channelsInfo: js.Object
+  def blendMode: js.Object
+  def groupLayer: js.Object
+  def infoKeys: js.Object
+  def top: Double
+  def left: Double
+  def bottom: Double
+  def right: Double
+  def channels: js.Object
+  def height: Double
+  def rows: js.Object
+  def width: Double
+  def cols: js.Object
+  def opacity: Double
+  def visible: Boolean
+  def clipped: js.Object
+  def layerEnd: js.Object
+  def legacyName: js.Object
+  def solidColor: js.Object
+  def vectorMask: js.Object
+  def vectorOrigination: js.Object
+  def typeTool: js.UndefOr[js.Function0[js.Object]]
+  def layerNameSource: js.Object
+  def layerId: js.Object
+  def blendClippingElements: js.Object
+  def blendInteriorElements: js.Object
+  def locked: js.Object
+  def metadata: js.Object
+  def image: js.Object
+  def node: PSDNode
+}
+
+@ScalaJSDefined
+trait PSDNodeExport extends js.Any {
+  def `type`: String
+  def visible: Boolean
+  def opacity: Double
+  def blendingMode: String
+  def name: String
+  def left: Double
+  def right: Double
+  def top: Double
+  def bottom: Double
+  def height: Double
+  def width: Double
+  def mask: js.UndefOr[PSDMask]
+  def text: js.UndefOr[PSDText]
+  def image: js.UndefOr[js.Object]
+}
+
+@ScalaJSDefined
+trait PSDHeader extends js.Any {
+  def file: PSDFile
+}
+
+@ScalaJSDefined
+trait PSDFile extends js.Any {
+  def data: js.Object
+}
+
+@ScalaJSDefined
+trait PSDMask extends js.Any {
+  def top: Double
+  def left: Double
+  def bottom: Double
+  def right: Double
+  def width: Double
+  def height: Double
+  def defaultColor: Double
+  def relative: Boolean
+  def disabled: Boolean
+  def invert: Boolean
+}
+
+@ScalaJSDefined
+trait PSDText extends js.Any {
+  def value: String
+  def font: PSDFont
+  def left: Double
+  def top: Double
+  def right: Double
+  def bottom: Double
+  def transform: PSDTransform
+}
+
+@ScalaJSDefined
+trait PSDFont extends js.Any {
+  def name: String
+  def sizes: js.Array[Double]
+  def colors: js.Array[js.Array[Double]]
+  def alignment: js.Array[String]
+}
+
+@ScalaJSDefined
+trait PSDTransform extends js.Any {
+  def xx: Double
+  def xy: Double
+  def yx: Double
+  def yy: Double
+  def tx: Double
+  def ty: Double
 }
--- src/main/scala/com/outr/psd/Test.scala
+++ src/main/scala/com/outr/psd/Test.scala
@@ -21,12 +21,40 @@ object Test extends js.JSApp {
       evt.stopPropagation()
       evt.preventDefault()
       PSD.fromEvent(evt).toFuture.foreach { psd =>
-        val tree = psd.tree()
-        val data = JSON.stringify(tree.export())
-        println(s"PSD: $data")
-        val image = document.getElementById("image");
-        image.appendChild(psd.image.toPng())
+        try {
+          println("Processing...")
+          val tree = psd.tree()
+          val image = document.getElementById("image")
+          image.appendChild(psd.image.toPng())
+
+          val data = document.getElementById("data")
+          data.innerHTML = JSON.stringify(tree.export(), space = 2)
+
+          val images = document.getElementById("images")
+          tree.descendants().toList.foreach { node =>
+            if (node.isGroup()) {
+              val div = document.createElement("div").asInstanceOf[html.Div]
+              div.innerHTML += s"<h3>${node.name} (group)</h3>"
+              images.appendChild(div)
+            } else {
+              val div = document.createElement("div").asInstanceOf[html.Div]
+              div.innerHTML += s"<h3>${node.name}</h3>"
+              val export = node.export()
+              export.text.foreach { text =>
+                div.innerHTML += s"<h4><b>Text:</b> ${text.value} (${text.font.name}, ${text.font.sizes}, ${text.font.colors}, ${text.font.alignment})</h4>"
+              }
+              div.appendChild(node.toPng())
+              images.appendChild(div)
+            }
+          }
+        } catch {
+          case t: Throwable => t.printStackTrace()
+        }
       }
     }, useCapture = true)
+  }
+
+  def describe(obj: js.Object): Unit = {
+
   }
 }
